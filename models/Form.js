106c85b27d705ca11a046b7f52026b66957e7364
Tejas <tejaswarambhe@gmail.com>
2022-07-08 04:55:24 +0530

    some changes

--- models/Form.js
+++ models/Form.js
@@ -5,409 +5,428 @@ const FormSchema = new Schema({
     //1) Profile Details
     //1A) Applied to office in City / Country
     present_rank: {
-        required: true,
+        // 
         type: String
     },
     rank_applied: {
-        required: true,
+        // 
         type: String
     },
     date_of_application: {
-        required: true,
+        // 
+        type: String
+    },
+    date_of_availability: {
+        // required : true,
+        type: String
+    },
+    agency: {
         type: String
     },
     //1B) Profile Details
     first_name: {
-        required: true,
+
         type: String
     },
     middle_name: {
-        required: true,
+
         type: String
     },
     last_name: {
-        required: true,
+
         type: String
     },
     date_of_birth: {
-        required: true,
+
         type: String
     },
     place_of_birth: {
-        required: true,
+
         type: String
     },
     nationality: {
-        required: true,
+
         type: String
     },
     maritial_status: {
-        required: true,
+
         type: String
     },
     smoker: {
-        required: true,
-        type: Boolean
+
+        type: String
     },
     permanent_address: {
-        required: true,
+
         type: String
     },
     present_address: {
-        requied: true,
+
         type: String
     },
     city_permanent: {
-        required: true,
+
         type: String
     },
     pin_code_permanent: {
-        required: true,
+
         type: Number
     },
     city_present: {
-        required: true,
+
         type: String
     },
     pin_code_present: {
-        required: true,
+
         type: Number
     },
     telephone_permanent: {
-        required: true,
+
         type: Number
     },
     email_permanent: {
-        required: true,
+
         type: String
     },
     telephone_present: {
-        required: true,
+
         type: Number
     },
     email_present: {
-        required: true,
+
         type: String
     },
     nearest_domestic_airport: {
-        required: true,
+
         type: String
     },
     nearest_international_airport: {
-        required: true,
+
         type: String
     },
     //2) NEXT OF KINS/ DEPENDENTS
     name_of_kin: {
-        required: true,
+
         type: String
     },
     relation: {
-        required: true,
+
         type: String
     },
     add_nkd: {
-        required: true,
+
         type: String
     },
     kin_array: {
-        required: true,
+
         type: Array
     },
     //3)Passport Details
     passport_no: {
-        required: true,
+
         type: Number
     },
     passport_place_of_issue: {
-        required: true,
+
         type: String
     },
     passport_date_of_issue: {
-        required: true,
+
         type: String
     },
     passport_date_of_expiry: {
-        required: true,
+
         type: String
     },
     us_visa_no: {
-        required: true,
+
         type: Number
     },
+    us_visa_place_of_issue: {
+
+        type: String
+    },
     us_visa_date_of_issue: {
-        required: true,
+
         type: String
     },
     us_visa_date_of_expiry: {
-        required: true,
+
         type: String
     },
     yellow_fever_no: {
-        required: true,
+
         type: Number
     },
+    yellow_fever_place_of_issue: {
+
+        type: String
+    },
     yellow_fever_date_of_issue: {
-        required: true,
+
         type: String
     },
     yellow_fever_date_of_expiry: {
-        required: true,
+
         type: String
     },
     //4)Continuous Discharge Certificate / Seaman Book
     indian_certificate_no: {
-        required: true,
+
         type: Number
     },
     indian_place_of_issue: {
-        required: true,
+
         type: String
     },
     indian_date_of_issue: {
-        required: true,
+
         type: String
     },
     indian_date_of_expiry: {
-        required: true,
+
         type: String
     },
     panama_certificate_no: {
-        required: true,
+
         type: Number
     },
     panama_date_of_issue: {
-        required: true,
+
         type: String
     },
     panama_date_of_expiry: {
-        required: true,
+
         type: String
     },
     others_certificate_no: {
-        required: true,
+
         type: Number
     },
     others_place_of_issue: {
-        required: true,
+
         type: String
     },
     others_date_of_issue: {
-        required: true,
+
         type: String
     },
     others_date_of_expiry: {
-        required: true,
+
         type: String
     },
     //5)Academic Qualifications
     elementary_name_of_institution: {
-        required: true,
+
         type: String
     },
     elementary_degree: {
-        required: true,
+
         type: String
     },
     elementary_from: {
-        required: true,
+
         type: Number
     },
     elementary_to: {
-        required: true,
+
         type: Number
     },
     secondary_name_of_institution: {
-        required: true,
+
         type: String
     },
     secondary_degree: {
-        required: true,
+
         type: String
     },
     secondary_from: {
-        required: true,
+
+        type: Number
+    },
+    secondary_to: {
+
         type: Number
     },
     university_name_of_institution: {
-        required: true,
+
         type: String
     },
     university_degree: {
-        required: true,
+
         type: String
     },
     university_from: {
-        required: true,
+
         type: Number
     },
     university_to: {
-        requirede: true,
+
         type: Number
     },
     professional_name_of_institution: {
-        required: true,
+
         type: String
     },
     professional_degree: {
-        required: true,
+
         type: String
     },
     professional_from: {
-        required: true,
+
         type: Number
     },
     professional_to: {
-        required: true,
+
         type: Number
     },
     additional_skills: {
-        required: true,
+
         type: String
     },
     //6) Liscence / Certificate of Competency
     indian_grade: {
-        required: true,
+
         type: String
     },
     indian_number: {
-        required: true,
+
         type: Number
     },
     indian_liscence_date_of_issue: {
-        required: true,
+
         type: String
     },
     indian_lisence_date_of_expiry: {
-        required: true,
+
         type: String
     },
     indain_lisence_date_revalidation: {
-        required: true,
+
         type: String
     },
     indian_stcw: {
-        required: true,
+
         type: Boolean
     },
     uk_grade: {
-        required: true,
+
         type: String
     },
     uk_number: {
-        required: true,
+
         type: Number
     },
     uk_liscence_date_of_issue: {
-        required: true,
+
         type: String
     },
     uk_lisence_date_of_expiry: {
-        required: true,
+
         type: String
     },
     uk_lisence_date_revalidation: {
-        required: true,
+
         type: String
     },
     uk_stcw: {
-        required: true,
+
         type: Boolean
     },
     aus_grade: {
-        required: true,
+
         type: String
     },
     aus_number: {
-        required: true,
+
         type: Number
     },
     aus_liscence_date_of_issue: {
-        required: true,
+
         type: String
     },
     aus_lisence_date_of_expiry: {
-        required: true,
+
         type: String
     },
     aus_lisence_date_revalidation: {
-        required: true,
+
         type: String
     },
     aus_stcw: {
-        required: true,
+
         type: Boolean
     },
     singapore_grade: {
-        required: true,
+
         type: String
     },
     singapore_number: {
-        required: true,
+
         type: Number
     },
     singapore_liscence_date_of_issue: {
-        required: true,
+
         type: String
     },
     singapore_lisence_date_of_expiry: {
-        required: true,
+
         type: String
     },
     singapore_lisence_date_revalidation: {
-        required: true,
+
         type: String
     },
     singapore_stcw: {
-        required: true,
+
         type: Boolean
     },
     panama_grade: {
-        required: true,
+
         type: String
     },
     panama_number: {
-        required: true,
+
         type: Number
     },
     panama_liscence_date_of_issue: {
-        required: true,
+
         type: String
     },
     panama_lisence_date_of_expiry: {
-        required: true,
+
         type: String
     },
     panama_lisence_date_revalidation: {
-        required: true,
+
         type: String
     },
     panama_stcw: {
-        required: true,
+
         type: Boolean
     },
     others_grade: {
-        required: true,
+
         type: String
     },
     others_number: {
-        required: true,
+
         type: Number
     },
     others_liscence_date_of_issue: {
-        required: true,
+
         type: String
     },
     others_lisence_date_of_expiry: {
-        required: true,
+
         type: String
     },
     others_lisence_date_revalidation: {
-        required: true,
+
         type: String
     },
     others_stcw: {
-        required: true,
+
         type: Boolean
     },
     //7)stcw and other certificates
@@ -612,25 +631,25 @@ const FormSchema = new Schema({
     },
     //9)How do you come to know about us
     company_presentation: {
-        type: Boolean
+        type: String
     },
     marine_club: {
-        type: Boolean
+        type: String
     },
     marine_magzine: {
-        type: Boolean
+        type: String
     },
     newspaper_advertisement: {
-        type: Boolean
+        type: String
     },
     told_by_seagoing_friend: {
-        type: Boolean
+        type: String
     },
     direct_mail: {
-        type: Boolean
+        type: String
     },
     others_advert: {
-        type: Boolean
+        type: String
     },
     //10)Personal Details
     height: {
@@ -649,19 +668,22 @@ const FormSchema = new Schema({
         type: String
     },
     any_major_surgery: {
-        type: Boolean
+        type: String
     },
     details_surgery: {
         type: String
     },
     defect_in_hearing: {
-        type: Boolean
+        type: String
     },
     defect_in_vision: {
-        type: Boolean
+        type: String
+    },
+    defect_in_speech: {
+        type: String
     },
     health_affected_by_climate: {
-        type: Boolean
+        type: String
     },
     health_affected_by_climate_reason: {
         type: String
@@ -670,7 +692,62 @@ const FormSchema = new Schema({
         type: Number
     },
     //11) Additional Information
-    applied_
+    applied_before: {
+        type: String
+    },
+    ever_bonded: {
+        type: String
+    },
+    crime_convicted: {
+        type: String
+    },
+    crime_convicted_details: {
+        type: String
+    },
+    ship_accident: {
+        type: String
+    },
+    ship_accident_details: {
+        type: String
+    },
+    certificate_suspended: {
+        type: String
+    },
+    certificate_suspended_from: {
+        type: String
+    },
+    certificate_suspended_to: {
+        type: String
+    },
+    //12 Reason For Application
+    leave_your_present_employment: {
+        type: String
+    },
+    anything_further_support: {
+        type: String
+    },
+    may_approach_employer: {
+        type: Number
+    },
+    employer_contact_details: {
+        type: Number
+    },
+    notice_to_attend_interview: {
+        type: String
+    },
+    //13)SEA Experience
+    special_experience: {
+        type: Array
+    },
+    //14)on shore services
+    on_shore: {
+        type: Array
+    },
+    //15) Declaration
+    declaration_date: {
+        type: String
+    },
+    //upload passport pic and signature
 
 
 
--- routes/upload.js
+++ routes/upload.js
@@ -1,12 +1,26 @@
 const express = require('express');
 const router = express.Router();
-
+const Form = require('../models/Form');
 
 router.get('/ping', (req, res) => {
     try {
         return res.json({
             success: "Pong"
         })
+    } catch (err) {
+        console.log(err.message);
+    }
+})
+
+router.post('/', async(req, res) => {
+    console.log(req.body);
+    try {
+        let form = await Form.create(req.body);
+        console.log(form);
+        res.status(200).json({
+            success: true,
+            form: form
+        });
     } catch (err) {
         console.log(err.message);
     }
